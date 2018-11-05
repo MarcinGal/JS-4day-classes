ca977649d4e32f5a80b4c453fe130c41e7ffc382
marcingal <marcin921125@gmail.com>
2018-11-05 10:04:31 +0100

    function Task added

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
- function ToDo(tasks) {
-    tasks = [];
+ function ToDo() {
+    this.tasks = [];
  }
 
-ToDo.prototype = addTask() {}
+ToDo.prototype.addTask = function (text) {
+    new Task(text)
+}
+
+function Task(text) {
+    this.text = text;
+}
 
