a224257386cb3db44adcae690030dded5974e5bb
marcingal <marcin921125@gmail.com>
2018-11-05 10:30:50 +0100

    tasks append in ul

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
- function ToDo() {
-    this.tasks = [];
- }
+function ToDo() {
+    this.tasks = []
+
+    this.render()
+}
 
 ToDo.prototype.addTask = function (text) {
-    new Task(text)
+    this.tasks.push(new Task(text))
+
+    this.render()
+}
+
+ToDo.prototype.render = function () {
+    document.body.innerHTML = ''
+
+    const ul = document.createElement('ul')
+
+    this.tasks.forEach(task => {
+        const li = document.createElement('li');
+        li.innerText = task.text
+        document.body.appendChild(ul);
+        ul.appendChild(li);
+    })
 }
 
 function Task(text) {
-    this.text = text;
+    this.text = text
 }
+
+const toDo1 = new ToDo()
+
+toDo1.addTask('Wynieś śmieci!')
+console.log(toDo1)
+
+
 
